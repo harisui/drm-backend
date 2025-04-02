3c6f29017149e98e6eba6a43d5a5b63c854f6243
Haris <haris1192317043@gmai.com>
2025-04-03 04:40:30 +0500

    new url

--- src/controllers/doctorSearch/rateMDControler.js
+++ src/controllers/doctorSearch/rateMDControler.js
@@ -15,7 +15,8 @@ const RateMDsController = {
             const allResults = [];
 
             do {
-                const url = `https://www.ratemds.com/best-doctors/?json=true&text=${encodeURIComponent(query)}&page=${currentPage}`;
+                // const url = `https://www.ratemds.com/best-doctors/?json=true&text=${encodeURIComponent(query)}&page=${currentPage}`;
+                const url = `https://www.ratemds.com/best-doctors/?json=true&text=${encodeURIComponent(query)}`;
                 const response = await axios.get(url, {
                     headers: {
                         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
